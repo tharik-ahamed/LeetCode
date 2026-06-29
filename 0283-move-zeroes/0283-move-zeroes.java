a4de9b1bf5594686414cbff797f8563d212a08d6
Tharik Ahamed <tharikahamedhajamydeen@gmail.com>
2026-06-29 10:40:58 +0530

    Time: 216 ms (5.39%), Space: 47.7 MB (56.46%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        for(int i=0;i<nums.length-1;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]==0){
+                    int temp=nums[j];
+                    nums[j]=nums[i];
+                    nums[i]=temp;
+                    
+                }
+            }
+        }
+    }
+}
