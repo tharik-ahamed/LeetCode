972fd22e508c8847f2b28be66237765076df5059
Tharik Ahamed <tharikahamedhajamydeen@gmail.com>
2026-06-29 10:16:43 +0530

    Time: 15 ms (20.65%), Space: 46.2 MB (35.09%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+     s=s.replaceAll("[^A-Za-z0-9]","").toLowerCase();
+     int l=0;
+     int r=s.length()-1;
+     while(l<r){
+        if(s.charAt(l)!=s.charAt(r)) return false;
+        l++;
+        r--;
+     }
+     return true;
+
+    }
+}
