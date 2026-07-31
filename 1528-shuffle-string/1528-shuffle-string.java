6cfd1584d938d27ee4e20e851388a867be109562
Tharik Ahamed <tharikahamedhajamydeen@gmail.com>
2026-07-31 19:26:22 +0530

    Time: 1 ms (64.24%), Space: 45.6 MB (57.69%) - LeetHub

--- 1528-shuffle-string/1528-shuffle-string.java
+++ 1528-shuffle-string/1528-shuffle-string.java
@@ -1,5 +1,11 @@
 class Solution {
     public String restoreString(String s, int[] indices) {
-        
+        char a[]=new char[s.length()];
+        StringBuilder sb=new StringBuilder("");
+        for(int i=0;i<s.length();i++){
+            a[indices[i]]=s.charAt(i);
+        }
+        sb.append(a);
+        return sb.toString();   
     }
 }
