f5de97d7abe81f6cc6030888fef40ee9c22a9bbe
Tharik Ahamed <tharikahamedhajamydeen@gmail.com>
2026-07-31 19:06:48 +0530

    Time: 1 ms (100%), Space: 85.5 MB (25.68%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        boolean visited [] = new boolean[nums.length+1];
+        for(int i:nums){
+            if(visited[i]){
+                return i;
+            }
+            visited[i]=true;
+        }
+        return 0;
+        
+    }
+}
